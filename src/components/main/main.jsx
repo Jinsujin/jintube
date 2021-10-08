1bea1f552aa9ebcb2de363a2772ee8875185a666
Jinsujin <sylpid003@gmail.com>
2021-10-08 17:28:00 +0900

    add reset.css and fetch api datalist

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppLayout from "../layout/appLayout";
+import VideoList from "../video_list/video_list";
 
-const Main = (props) => <AppLayout>main</AppLayout>;
+const Main = (props) => {
+  const [videos, setVideos] = useState([]);
+
+  //[] 빈 배열 옵션은, 마운트될떄 한번만 호출
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=16&q=bts&type=video&key=AIzaSyBvhXba4bE-2qoubJPlyoa4Vo9Zv_CbKw4",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <AppLayout>
+      <VideoList videos={videos} />
+    </AppLayout>
+  );
+};
 
 export default Main;
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <div>video item</div>;
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.wrap}>
+    <div className={styles.video}>
+      <img src={snippet.thumbnails.medium.url} alt="thumbnail" />
+      <p>{snippet.title}</p>
+      <p>{snippet.channelTitle}</p>
+      <p>{snippet.publishedAt}</p>
+    </div>
+  </li>
+);
 
 export default VideoItem;
