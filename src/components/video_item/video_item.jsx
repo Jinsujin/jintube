14d3a6bb7ef438b2e8a1dcc7a328badcc0ec93f2
Jinsujin <sylpid003@gmail.com>
2021-10-08 20:18:22 +0900

    edit video_item.css

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -7,18 +7,20 @@ const VideoItem = ({ video: { snippet } }) => (
       <img src={snippet.thumbnails.medium.url} alt="thumbnail" />
       <div className={styles.details}>
         <div className={styles.avatar}>
-          <div className={styles.circle}>
-            <span>{snippet.channelTitle.charAt(0)}</span>
-          </div>
+          <div className={styles.circle}>{snippet.channelTitle.charAt(0)}</div>
         </div>
         <div className={styles.meta}>
           <p className={styles.title}>{snippet.title}</p>
           <p className={styles.channel}>{snippet.channelTitle}</p>
-          <p className={styles.publishedAt}>{snippet.publishedAt}</p>
+          <p className={styles.publishedAt}>
+            {snippet.publishedAt.substring(0, 10)}
+          </p>
         </div>
       </div>
     </div>
   </li>
 );
+
+// 2021-10-08T09:00:25Z
 
 export default VideoItem;
