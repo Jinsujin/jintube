20f63688fa01f0d8b5de9a986d3fd9f09c30d9f1
Jinsujin <sylpid003@gmail.com>
2021-10-08 19:50:04 +0900

    style video_item component

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -5,9 +5,18 @@ const VideoItem = ({ video: { snippet } }) => (
   <li className={styles.wrap}>
     <div className={styles.video}>
       <img src={snippet.thumbnails.medium.url} alt="thumbnail" />
-      <p>{snippet.title}</p>
-      <p>{snippet.channelTitle}</p>
-      <p>{snippet.publishedAt}</p>
+      <div className={styles.details}>
+        <div className={styles.avatar}>
+          <div className={styles.circle}>
+            <span>{snippet.channelTitle.charAt(0)}</span>
+          </div>
+        </div>
+        <div className={styles.meta}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+          <p className={styles.publishedAt}>{snippet.publishedAt}</p>
+        </div>
+      </div>
     </div>
   </li>
 );
